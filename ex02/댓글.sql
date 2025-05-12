31c6faef16506a34388ede0efb8ffb9cfb76f70d
bbolung <shoh2802@gmail.com>
2025-05-12 17:56:32 +0900

    *

---
+++ ex02/댓글.sql
@@ -0,0 +1,35 @@
+-- 댓글
+create table tbl_reply(
+    rno number(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000) not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date default sysdate
+);
+
+create SEQUENCE seq_reply;
+
+-- rno 기본키 설정(이름 : pk_reply)
+alter table tbl_reply
+add CONSTRAINT pk_reply primary key(rno);
+
+-- bno : 외래키 설정(이름 : fk_reply_board)
+alter table tbl_reply
+add CONSTRAINT fk_reply_board FOREIGN key(bno) REFERENCES tbl_board(bno);
+
+-- 내림차순으로 10개 데이터 조회
+select *
+from tbl_board
+where rownum < 10
+order by bno desc;
+
+-- 추가한 댓글Test 확인
+select *
+from tbl_reply;
+
+-- 전체 데이터 조회 sql문
+select * 
+from tbl_reply 
+where bno = 786442 
+order by rno desc;
